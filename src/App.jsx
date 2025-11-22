b007030f81142e869248f91a3b88aa69f980a5fc
Abdelrhman <lito2182002@gmail.com>
2025-11-23 00:05:25 +0200

    establish the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-800">Welcome to the Library Management System</h1>
+    </>
+  )
+}
+
+export default App
