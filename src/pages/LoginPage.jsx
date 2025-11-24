d6277275cb0d43cbcd66b2e87be58efdcf025d34
Abdelrhman <lito2182002@gmail.com>
2025-11-24 22:59:30 +0200

    file management

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,9 @@
-    import { LoginForm } from '../components/LoginForm'
+import { LoginForm } from "../components/login/LoginForm";
 export default function LoginPage() {
-    return (    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 flex items-center justify-center px-4 py-8 sm:px-6 lg:px-8">  <LoginForm /></div>)}
+  return (
+    <div className="min-h-screen bg-linear-to-br from-gray-50 to-gray-100 flex items-center justify-center px-4 py-8 sm:px-6 lg:px-8">
+      {" "}
+      <LoginForm />
+    </div>
+  );
+}
--- src/pages/SignUPPage.jsx
+++ src/pages/SignUPPage.jsx
@@ -1,5 +1,8 @@
-import { SignUpForm } from "../components/SignUpForm";
+import { SignUpForm } from "../components/signup/SignUpForm";
 export default function SignUPPage() {
-    return (    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 flex justify-center px-4 py-8 sm:px-6 lg:px-8"> 
-     <SignUpForm /></div>)
+  return (
+    <div className="min-h-screen bg-linear-to-br from-gray-50 to-gray-100 flex justify-center px-4 py-8 sm:px-6 lg:px-8">
+      <SignUpForm />
+    </div>
+  );
 }
